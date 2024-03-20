9f975a38091da52004e86c75246f2e28d597c0b4
connor <connord79@gmail.com>
2024-03-20 16:01:53 +1100

    First Commit - Draft Program

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import requests
-from constants import API_KEY
+from constants import API_KEY, URL
 import csv
 
-# API Url
-URL = 'https://clientculture.net/api/quarterly_results'
 
-# Make requests
-
-headers = {
+# Create Header for the API request
+HEADERS = {
     'x-api-secret': API_KEY,
 }
 
-response = requests.get(URL, headers=headers)
+
+# Make requests
+response = requests.get(URL, headers=HEADERS)
+
 
 if response.status_code == 200:
+    # Date to .json
     data = response.json()
 
-
+    # Get the keys from the response
     keys = data[0].keys()
-    csv_filename = 'output.csv'
 
+    # Create the CSV
+    csv_filename = 'ClientSense.csv'
+
+    # Open the CSV file
     with open(csv_filename, 'w', newline='') as csvfile:
+        # Write the data to a .csv file
         writer = csv.DictWriter(csvfile, fieldnames=keys)
         writer.writeheader()
         for row in data:
             writer.writerow(row)
 
+    # Error Handling
     print(f"CSV file '{csv_filename}' created successfully.")
 else:
     print(f"Failed to call API. Status code: {response.status_code}")
