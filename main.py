910f656a7f21f558a05c7eea50c98ca86a4aec9f
connor <connord79@gmail.com>
2024-03-20 15:56:56 +1100

    First Commit - Draft Program

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from constants import API_KEY
+import csv
+
+# API Url
+URL = 'https://clientculture.net/api/quarterly_results'
+
+# Make requests
+
+headers = {
+    'x-api-secret': API_KEY,
+    'value': ''
+}
+
+response = requests.get(URL, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+
+
+    keys = data[0].keys()
+    csv_filename = 'output.csv'
+
+    with open(csv_filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=keys)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
+    print(f"CSV file '{csv_filename}' created successfully.")
+else:
+    print(f"Failed to call API. Status code: {response.status_code}")
+    print(response.text)
+
+# print(content)
